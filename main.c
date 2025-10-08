afa89c757543e1f724beb9f3c0e0b6c72c1549cf
tac1DEV <sansberrot@gmail.com>
2025-10-08 14:27:15 +0200

    if statements

--- main.c
+++ main.c
@@ -1,40 +1,14 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
     
-    float x = 2.1;
+    int age = 21;
 
-    // x = sqrt(x);
-    // x = pow(x, 2);
-    // x = round(x);
-    //entier supérieur
-    // x = ceil(x);
-    //entier inférieur
-    // x = floor(x);
-    // x = abs(x);
-    // x = log(x);
-    // x = sin(x);
-    // x = cos(x);
-    // x = tan(x);
-
-    double radius = 0.0;
-    double area = 0.0;
-    double surfaceArea = 0.0;
-    double volume = 0.0;
-    const double PI = 3.14159;
-
-
-    printf("Enter the radius: ");
-    scanf("%lf", &radius);
-
-    area = PI * pow(radius, 2);
-    surfaceArea = 4 * PI * pow(radius, 2);
-    volume = (4.0/3.0) * PI * pow(radius, 3);
-
-    printf("Area: %.2lf\n", area);
-    printf("Surface Area: %.2lf\n", surfaceArea);
-    printf("Volume: %.2lf\n", volume);
+    if (age >= 18) {
+        printf("You are an adult.\n");
+    } else {
+        printf("You are a minor.\n");
+    }
 
     return 0;
 }
