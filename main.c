f597f993e3cab80a1f9e6c9d2765f031e93ce31a
tac1DEV <sansberrot@gmail.com>
2025-10-08 14:35:07 +0200

    switches

--- main.c
+++ main.c
@@ -2,13 +2,31 @@
 
 int main() {
     
-    int age = 21;
+    int dayOfWeek = 5;
 
-    if (age >= 18) {
-        printf("You are an adult.\n");
-    } else {
-        printf("You are a minor.\n");
+    switch (dayOfWeek) {
+        case 0:
+            printf("Sunday\n");
+            break;
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        default:
+            printf("Invalid day\n");
     }
-
-    return 0;
 }
