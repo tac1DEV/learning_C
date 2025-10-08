b48c840070274fb14b627b7fccf7b37ff4bfd042
tac1DEV <sansberrot@gmail.com>
2025-10-08 17:27:49 +0200

    banking program

--- main.c
+++ main.c
@@ -1,27 +1,83 @@
 #include <stdio.h>
 
-void happyBirthday(char name[], int age) {
-    printf("Happy Birthday to You!\n");
-    printf("Happy Birthday to You!\n");
-    printf("Happy Birthday Dear %s!\n", name);
-    if(age >= 30) {
-    printf("Happy Birthday to You elder!\n");
-    }else{
-    printf("Happy Birthday to You %d years old person!\n", age);
-    }
-}
+void checkBalance(float balance);
+float deposit();
+float withdraw(float balance);
 
 int main() {
 
-    char name[] = "";
-    int age = 0;
+    int choice = 0;
+    float balance = 0.0f;
 
-    printf("Enter name: ");
-    scanf("%s", &name);
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    
+    printf("*** Welcome to the Bank ***");
 
-    happyBirthday(name, age);
+    do{
+        printf("\nSelect an option:\n");
+        printf("\n1. Check Balance\n");
+        printf("2. Deposit Money\n");
+        printf("3. Withdraw Money\n");
+        printf("4. Exit\n");
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            checkBalance(balance);
+            break;
+        case 2:
+            balance += deposit();
+            break;
+        case 3:
+            balance -= withdraw(balance);
+            break;
+        case 4:
+            printf("\nThank you for using the bank!\n");
+            break;
+        default:
+            printf("\nInvalid choice. Please try again.\n");
+            break;
+        }
+
+    }while(choice != 4);
+
     return 0;
 }
+
+void checkBalance(float balance){
+    printf("\nYour current balance is: $%.2f\n", balance);
+};
+float deposit(){
+    
+    float amount = 0.0f;
+
+    printf("\nEnter the amount to deposit: $");
+    scanf("%f", &amount);
+
+    if(amount <= 0.0){
+        printf("\nInvalid amount.\n");
+        return 0.0f;
+    }else{
+        printf("Successfully deposited $%.2f\n", amount);
+        return amount;
+    }
+
+    return 0.0f;
+};
+float withdraw(float balance){
+        float amount = 0.0f;
+
+    printf("\nEnter the amount to withdraw: $");
+    scanf("%f", &amount);
+
+    if(amount <= 0.0){
+        printf("\nInvalid amount.\n");
+        return 0.0f;
+    }else if(amount > balance){
+        printf("Insufficient funds. Your current balance is $%.2f\n", balance);
+        return 0.0f;
+    }else{
+        printf("Successfully withdrew $%.2f\n", amount);
+        return amount;
+    }
+};
