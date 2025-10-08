9f94d407633128b671967b761274753c17985b14
tac1DEV <sansberrot@gmail.com>
2025-10-08 13:57:11 +0200

    shopping cart

--- main.c
+++ main.c
@@ -2,40 +2,26 @@
 #include <string.h>
 
 int main() {
-    // printf("Hello, World!\n");
+    char item[50] = "";
+    float price = 0.0f;
+    int quantity = 0;
+    char currency = '$';
+    float total = 0.0f;
 
-    // int a = 5;
-    // float b = 2.5;
-    // double pi = 3.14159265358979;
+    printf("What would you like to buy ?: ");
+    fgets(item, sizeof(item), stdin);
+    item[strlen(item) - 1] = '\0'; // Remove newline character
+    
+    printf("What is the price ?: ");
+    scanf("%f", &price);
+    
+    printf("What is the quantity ?: ");
+    scanf("%d", &quantity);
 
-    // printf("Hello, %d!\n", a);
-    // printf("Ce produit coute %f€!\n", b);
-    // printf("Pi vaut environ %.2f!\n", pi);
-    // printf("Pi vaut environ %.15lf!\n", pi);
+    total = price * quantity;
 
-    int a = 0;
-    float b=0.0f;
-    char c = '\0';
-    char d[30] = "";
-
-    //On assigne une valeur à l'emplacement mémoire de a
-    printf("Entrez un entier: ");
-    scanf("%d", &a);
-    printf("Entrez un decimal: ");
-    scanf("%f", &b);
-    printf("Entrez une lettre: ");
-    // Ajouter un espace avant %c pour eviter de capturer le caractère de nouvelle ligne
-    scanf(" %c", &c);
-
-    getchar(); // Pour clear le caractère de nouvelle ligne restant dans le buffer
-    printf("Entrez deux mots de (30 carac. max): ");
-    fgets(d, sizeof(d), stdin);
-    d[strlen(d) - 1] = '\0'; // Enlever le caractère de nouvelle ligne à la fin
-
-    printf("%s\n", d);
-    printf("%d\n", a);
-    printf("%.2f\n", b);
-    printf("%c\n", c);
+    printf("You have bought %d %s\n", quantity, item);   
+    printf("The total is: %c%.2f", currency, total);   
 
     return 0;
 }
